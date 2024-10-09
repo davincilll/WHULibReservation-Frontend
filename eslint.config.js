079871a150da3674ee78bdaba7d1f389ea9f522c
NSCola <3135287831@qq.com>
2024-10-09 17:20:18 +0800

    feat: 添加了eslint规则和nuxt start 命令

--- eslint.config.js
+++ eslint.config.js
@@ -11,4 +11,10 @@ export default [
     ...tseslint.configs.recommended,
     ...pluginVue.configs["flat/essential"],
     {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+    // 添加规则禁用
+    {
+        rules: {
+            "vue/multi-word-component-names": "off", // 禁用多词组件名称规则
+        },
+    },
 ];
